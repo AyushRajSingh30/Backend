38b4c0ee36835a930be73435891fd6f3c17972ca
ayush_raj <ayushrajsin30@gmail.com>
2024-01-20 09:00:11 +0530

    utility make

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+
+const app=express()
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+})) 
+
+//data come in json form and we set limit  which limit data accepted
+app.use(express.json({limit:"16kb"}))
+
+//data come from url and we set limit  urlencoded encode url in better form
+
+app.use(express.urlencoded({extended:true, limit:"16kb"}))
+
+//static used for static file like image and so more 
+
+app.use(express.static("public"))
+
+//set cookie at user side this is used for CRUD opreations
+app.use(cookieParser())
+
+
+export {app};
--- src/index.js
+++ src/index.js
@@ -9,6 +9,30 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("ERROR", error);
+        throw error;
+       })
+
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is running on port : ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("MONGO ERROR connection fail !!!", error);
+
+})
+
+
+
+
+
+
+
+
+
+
 
 
 
