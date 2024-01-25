ed693be9b6c3bf7c3cfc93edebc88fbf8307f976
ayush_raj <ayushrajsin30@gmail.com>
2024-01-26 04:19:21 +0530

    file uplode

--- src/models/user.module.js
+++ src/models/user.module.js
@@ -50,18 +50,27 @@ const userSchema =new Schema({
 },{timestamps:true})
 
   
+/*In Mongoose, pre middleware allows you to define functions that run before specific operations such as saving or updating documents in the database.Why Use it?: You can use pre middleware to perform custom actions or modify data before it gets stored in the database.*/
+
 userSchema.pre("save",  async function(next){
-//if cheak password is modifi or not if it modify then than password save and encreypt
+//If cheak password is modifi or not if it modify then than password save and encreypt
     if(!this.isModified("password")) return next() 
     this.password=bcrypt.hash(this.password, 10)
 //10 is limit
     next()
 })
+
 //this method cheak password is true or false
+
 userSchema.method.isPasswordCorrect=async function(password){
-    //password is given by user  and this.password is encreypt password 
+/*pasword compare we give to input (1. user given password) && (2.bcrypt encrypt this.password)*/
 return await bcrypt.compare(password, this.password)
 }
+
+
+
+// The primary purpose of a refresh token is to renew or obtain a new access token without requiring the user to log in again.
+
 
 userSchema.method.generateAccessToken=async function() { 
     return jwt.sign({
---
+++ src/utils/cloudnary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+       
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
+
+
+const uplodeOnCloudinary= async (localFilePath)=>{
+     try {
+        if(!localFilePath) return null
+        //uplode the file on Cloudinary
+   const response=await cloudinary.uploader.upload(localFilePath, {resource_type:"auto"})
+        //file has been uploded successfully
+        console.log("Fill is uploded on cloudinary", response.url);
+        return response;
+        
+     } catch (error) {
+        fs.unlinkSync(localFilePath) //remove the locialy save tempory file as the uplode operation got failed
+        return null;
+        
+     } 
+}
+
+export {uplodeOnCloudinary};
